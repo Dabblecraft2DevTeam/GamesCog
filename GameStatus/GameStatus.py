36ad85657b8252da6e1122d2fe4b86487bc0e6aa
keough99 <DabblecraftInc@outlook.com>
2018-11-17 21:06:42 -0500

    Update GameStatus.py

--- GameStatus/GameStatus.py
+++ GameStatus/GameStatus.py
@@ -8,13 +8,16 @@ class GamesCog:
 
     def __init__(self, bot):
         self.bot = bot
+    @bot.event()
+    async def change_status(self):
+        l = ["Do \help for help", "I am the walrus", "To kill a mocking bird", "Living is easy with eyes closed, Misunderstanding all you see"]
+        await self.change_presence(activity=discord.Game(name=random.choice(l)))
+        await asyncio.sleep(60)
 
-async def change_status(self):
- l = ["Do \help for help", "I am the walrus", "To kill a mocking bird", "Living is easy with eyes closed", "Misunderstanding all you see"]
- await self.change_presence(activity=discord.Game(name=random.choice(l)))
- await asyncio.sleep(60)
-@bot.event
-async def on_ready(self):
- self.loop.create_task(change_status())
+
+    async def on_ready(self):
+        print(f'Username: {self.user.name}')
+        print(f'Client ID: {self.user.id}')
+        self.bg_task = self.loop.create_task(self.change_status())
 def setup(bot):
     bot.add_cog(GamesCog(bot))
